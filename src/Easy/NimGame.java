889fb5dc14e888f5d44c877663d788f3ad1c9886
Heiku <1154034741@qq.com>
2018-04-25 09:29:08 +0800

    add NimGame

---
+++ src/Easy/NimGame.java
@@ -0,0 +1,14 @@
+package Easy;
+
+/**
+ * 解题思路：
+ * 找到非4的倍数的数
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0)
+            return false;
+        else
+            return true;
+    }
+}
