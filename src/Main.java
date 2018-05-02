dc69c2344c10e3374c6ebb3017e45e0098159d34
Heiku <1154034741@qq.com>
2018-05-02 11:23:14 +0800

    add TitleToNumber

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s = new String("heiku.github.io");
+        /*String s = new String("heiku.github.io");
         String []arr = s.split("\\.");
         for (String str : arr){
             System.out.println(str);
-        }
+        }*/
+
+        System.out.println((701-25) % 26);
     }
 }
