c363a12c896efbd0685f8ea0c0b86d3e21ac85b8
Heiku <1154034741@qq.com>
2018-05-09 22:45:27 +0800

    add DistributeCandies

---
+++ src/Easy/DistributeCandies.java
@@ -0,0 +1,31 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DistributeCandies {
+    public int distributeCandies(int[] candies) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int candy : candies){
+            if (map.containsKey(candy)){
+                int val = map.get(candy);
+                val++;
+                map.replace(candy, val);
+            }else {
+                map.put(candy, 1);
+            }
+        }
+
+        int len = candies.length;
+        int diff = map.size();
+        return Math.min(diff, len / 2);
+    }
+
+
+    public static void main(String[] args) {
+        int []candies = new int[]{1000,1,1,1};
+        DistributeCandies d = new DistributeCandies();
+        d.distributeCandies(candies);
+    }
+}
