e33442da2761d2483f6be2cd699aa9e0de5e52c8
Heiku <1154034741@qq.com>
2018-05-14 13:12:23 +0800

    add FindMaxConsecutiveOnes

---
+++ src/Easy/FindMaxConsecutiveOnes.java
@@ -0,0 +1,29 @@
+package Easy;
+
+public class FindMaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 1){
+                count++;
+                if (count > max)
+                    max = count;
+            }else {
+                if (count > max){
+                    max = count;
+                }
+                count = 0;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int []n= new int[]{1,1,0,1};
+        FindMaxConsecutiveOnes f = new FindMaxConsecutiveOnes();
+        f.findMaxConsecutiveOnes(n);
+    }
+}
