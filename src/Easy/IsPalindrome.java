b68ea528e9f9b6ebe9fa75464f434354666d86f5
Heiku <1154034741@qq.com>
2018-06-04 12:45:04 +0800

    add CountPrimeSetBits & FindUSLength & isPalindorme

---
+++ src/Easy/IsPalindrome.java
@@ -0,0 +1,35 @@
+package Easy;
+
+/**
+ * 9
+ * 题目：判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 思路：逆序一半，判断特殊值
+ */
+public class IsPalindrome {
+
+    // 1221 121
+    public boolean isPalindrome(int x) {
+
+        // 除去负数和尾数为0的数
+        if (x < 0 || (x % 10 == 0) && (x != 0))
+            return false;
+
+        int reverseNum = 0;
+        while (x > reverseNum){
+            reverseNum = reverseNum * 10 +  x % 10;
+            x /= 10;
+        }
+
+        // 数字位为偶数或奇数
+        if (x == reverseNum || x == (reverseNum / 10))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        IsPalindrome isPalindrome = new IsPalindrome();
+        isPalindrome.isPalindrome(121);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,6 @@ public class Main {
         }*/
 
         /*System.out.println((701-25) % 26);*/
-        System.out.println(0^ 1);
+        System.out.println(0 %  10);
     }
 }
